144fe9545b40937d28e2adb9dd3331033c3315a6
sribalaji-16 <sribalajimuthuraman@outlook.com>
2024-10-14 09:28:51 +0530

    Update gates.vhd

--- gates.vhd
+++ gates.vhd
@@ -6,7 +6,6 @@
 -- THIS WORK CONTAINS TRADE SECRET AND PROPRIETARY INFORMATION WHICH IS THE PROPERTY OF 
 -- MENTOR GRAPHICS CORPORATION OR ITS LICENSORS AND IS SUBJECT TO LICENSE TERMS.
 --   
-
 ------------------------------------------------------------------------
 -- package with component declarations
 ------------------------------------------------------------------------
